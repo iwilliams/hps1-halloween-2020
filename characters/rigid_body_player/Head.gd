16fa213f338637c54c4410fbd081ccec6f8dd7d5
Ian Williams <ian@iwillia.ms>
2020-10-10 00:42:17 -0700

    Backwards PD node

--- characters/rigid_body_player/Head.gd
+++ characters/rigid_body_player/Head.gd
@@ -46,7 +46,6 @@ func _integrate_forces(state: PhysicsDirectBodyState):
         grab(grab_on_next_frame)
         grab_on_next_frame = null
         return
-            
     if !holding && Input.is_action_just_pressed("grab") && collider is RigidBody:
         state.transform = state.transform.looking_at(collider.global_transform.origin, Vector3.UP)
         grab_on_next_frame = collider
@@ -58,88 +57,15 @@ func _integrate_forces(state: PhysicsDirectBodyState):
         holding.mode = RigidBody.MODE_STATIC
         drop()
         mode = RigidBody.MODE_CHARACTER
+
     if mode == RigidBody.MODE_RIGID:
         state.transform.origin = pitch.global_transform.origin
-        error_correct_backwards_pd(state)
+        ($BackwardsPD as BackwardsPD).set_torque(
+                state.transform.basis.get_rotation_quat(), 
+                pitch.global_transform.basis.get_rotation_quat(),
+                state,
+                self,
+                holding != null and (holding as RigidBody).get_colliding_bodies().size()
+        )
     else:
         state.transform = pitch.global_transform
-
-
-# https://digitalopus.ca/site/pd-controllers/
-func error_correct_backwards_pd(state: PhysicsDirectBodyState) -> void:
-    var frequency = 60
-    var damping = .5
-    
-    var kp = (6*frequency)*(6*frequency)* 0.25
-    var kd = 4.5*frequency*damping
-    var dt = state.step
-    var g: float = 1 / (1 + kd * dt + kp * dt * dt)
-    var ksg: float = kp * g
-    var kdg: float = (kd + kp * dt) * g
-    var q: Quat = get_rotation_error()
-    
-    if q.w < 0:
-        q.x = -q.x
-        q.y = -q.y
-        q.z = -q.z
-        q.w = -q.w
-
-    var x := Vector3()
-    var xMag: float
-        
-    # https://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToAngle/index.htm
-    var q1: Quat = Quat(q.x, q.y, q.z, q.w)
-    if q1.w > 1:
-        q1 = q1.normalized() # if w>1 acos and sqrt will produce errors, this cant happen if quaternion is normalised
-    xMag = 2 * acos(q1.w);
-    var s: float = sqrt(1-q1.w*q1.w); # assuming quaternion normalised then w is less than 1, so term always positive.
-    if s < 0.001: # test to avoid divide by zero, s is always positive due to sqrt
-        # if s close to zero then direction of axis not important
-        x = Vector3(q1.x, q1.y, q1.z) # if it is important that axis is normalised then replace with x=1; y=z=0;
-    else:
-        x = Vector3(q1.x / s, q1.y / s, q1.z / s)
-    
-    var is_colliding = holding != null and (holding as RigidBody).get_colliding_bodies().size()
-    if xMag < .1 && !is_colliding:
-        state.transform = pitch.global_transform
-    
-    x = x.normalized()
-    x = Vector3(deg2rad(x.x), deg2rad(x.y), deg2rad(x.z))
-    
-    var pidv: Vector3 = kp * x * xMag - kd * angular_velocity
-    var rotInertia2World: Quat = global_transform.basis.get_rotation_quat() * get_inverse_inertia_tensor().inverse().get_rotation_quat() # inverse the inverse?? seems like we need to?
-    pidv = rotInertia2World.inverse() * pidv
-    pidv *= state.inverse_inertia # inverse the inverse?, doesn't seem like we need to
-    pidv = rotInertia2World * pidv
-    var maximum = 50
-    if is_colliding:
-        maximum = 10
-    pidv = Vector3(clamp(pidv.x, -maximum, maximum), clamp(pidv.y, -maximum, maximum), clamp(pidv.z, -maximum, maximum))
-    add_torque(pidv)
-    
-
-func error_correct_basic(state: PhysicsDirectBodyState) -> void:
-    var diff: Quat = get_rotation_error()
-    var eulers = orientTorque(diff.get_euler())
-    var torque = eulers
-    if torque.length() < 0.2:
-        state.angular_velocity = Vector3.ZERO
-    else:
-        torque = global_transform.basis.get_rotation_quat() * torque * 20
-    add_torque(torque)
-
-
-func get_rotation_error() -> Quat:
-    var diff: Quat = pitch.global_transform.basis.get_rotation_quat() * global_transform.basis.get_rotation_quat().inverse()
-    return diff
-
-
-func orientTorque(torque: Vector3) -> Vector3:
-    var newTorque = Vector3(torque.x, torque.y, torque.z)
-    if torque.x > 180:
-        newTorque = torque.x - 360
-    if torque.y > 180:
-        newTorque = torque.y - 360
-    if torque.z > 180:
-        newTorque = torque.z - 360
-    return newTorque
---
+++ utils/BackwardsPD.gd
@@ -0,0 +1,71 @@
+class_name BackwardsPD
+extends Node
+
+
+export(float) var frequency = 1 setget _set_frequency
+func _set_frequency(value):
+    frequency = value
+    kp = (6*frequency)*(6*frequency)* 0.25
+
+
+export(float) var damping = 1 setget _set_damping
+func _set_damping(value):
+    damping = value
+    kd = 4.5*frequency*damping
+
+export(float) var threshold = .1
+
+var kp = (6*frequency)*(6*frequency)* 0.25
+var kd = 4.5*frequency*damping
+
+
+func get_rotation_error(current_rotation: Quat, desired_rotation: Quat) -> Quat:
+    var error: Quat = desired_rotation * current_rotation.inverse()
+    return error
+
+
+# https://digitalopus.ca/site/pd-controllers/
+func set_torque(current_rotation: Quat, desired_rotation: Quat, state: PhysicsDirectBodyState, body: RigidBody, is_colliding := false) -> void:   
+    var dt = state.step
+    var g: float = 1 / (1 + kd * dt + kp * dt * dt)
+    var ksg: float = kp * g
+    var kdg: float = (kd + kp * dt) * g
+    var q: Quat = get_rotation_error(current_rotation, desired_rotation)
+    
+    if q.w < 0:
+        q.x = -q.x
+        q.y = -q.y
+        q.z = -q.z
+        q.w = -q.w
+
+    var x := Vector3()
+    var xMag: float
+        
+    # https://www.euclideanspace.com/maths/geometry/rotations/conversions/quaternionToAngle/index.htm
+    var q1: Quat = Quat(q.x, q.y, q.z, q.w)
+    if q1.w > 1:
+        q1 = q1.normalized() # if w>1 acos and sqrt will produce errors, this cant happen if quaternion is normalised
+    xMag = 2 * acos(q1.w);
+    var s: float = sqrt(1-q1.w*q1.w); # assuming quaternion normalised then w is less than 1, so term always positive.
+    if s < 0.001: # test to avoid divide by zero, s is always positive due to sqrt
+        # if s close to zero then direction of axis not important
+        x = Vector3(q1.x, q1.y, q1.z) # if it is important that axis is normalised then replace with x=1; y=z=0;
+    else:
+        x = Vector3(q1.x / s, q1.y / s, q1.z / s)
+    
+    if xMag < threshold && !is_colliding:
+        state.transform.basis = desired_rotation
+    
+    x = x.normalized()
+    x = Vector3(deg2rad(x.x), deg2rad(x.y), deg2rad(x.z))
+    
+    var pidv: Vector3 = kp * x * xMag - kd * body.angular_velocity
+    var rotInertia2World: Quat = body.global_transform.basis.get_rotation_quat() * body.get_inverse_inertia_tensor().inverse().get_rotation_quat() # inverse the inverse?? seems like we need to?
+    pidv = rotInertia2World.inverse() * pidv
+    pidv *= state.inverse_inertia # inverse the inverse?, doesn't seem like we need to
+    pidv = rotInertia2World * pidv
+    var maximum = 50
+    if is_colliding:
+        maximum = 10
+    pidv = Vector3(clamp(pidv.x, -maximum, maximum), clamp(pidv.y, -maximum, maximum), clamp(pidv.z, -maximum, maximum))
+    body.add_torque(pidv)
