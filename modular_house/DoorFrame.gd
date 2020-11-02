d4781e517429739ef8ccaac4a3bbf9e34254d0f5
Ian Williams <ian@iwillia.ms>
2020-11-02 14:06:06 -0800

    Modular house mostly done

---
+++ modular_house/DoorFrame.gd
@@ -0,0 +1,9 @@
+extends Spatial
+
+
+func _ready():
+    $Door.add_collision_exception_with($Stairs)
+    $Door.add_collision_exception_with($WoodPanel)
+    $Door.add_collision_exception_with($WoodPanel2)
+    $Door.add_collision_exception_with($DoorPanel)
+    $Door.add_collision_exception_with($DoorPanel2)
