b470de8b941b93432f2c7d4f89a76cc1ca948be4
Ian Williams <ian@iwillia.ms>
2020-11-08 15:24:26 -0800

    Total house destruction

--- modular_house/DoorFrame.gd
+++ modular_house/DoorFrame.gd
@@ -11,5 +11,32 @@ func unlock():
     $Door.unlock()
 
 
-func destroy_door():
-    pass
+func destroy_door(b = null):
+    for i in range(0, 4 - break_count):
+        break_wall()
+        yield(get_tree().create_timer(1), "timeout")
+
+    $FrameTop.mode = RigidBody.MODE_RIGID
+    $FrameTop.apply_torque_impulse(Vector3(0, .2, 0))
+
+    yield(get_tree().create_timer(.2), "timeout")
+    $FrameLeft.mode = RigidBody.MODE_RIGID
+    $FrameLeft.apply_torque_impulse(Vector3(0, 0, .2))
+    
+    yield(get_tree().create_timer(.2), "timeout")
+    $FrameRight.mode = RigidBody.MODE_RIGID
+    $FrameRight.apply_torque_impulse(Vector3(0, 0, -.2))
+    
+    yield(get_tree().create_timer(.2), "timeout")
+    $FrameRightMiddle.mode = RigidBody.MODE_RIGID
+    $FrameRightMiddle.apply_torque_impulse(Vector3(0, 0, -.2))
+    
+    yield(get_tree().create_timer(.2), "timeout")
+    $FrameLeftMiddle.mode = RigidBody.MODE_RIGID
+    $FrameLeftMiddle.apply_torque_impulse(Vector3(0, 0, -.2))
+    
+    yield(get_tree().create_timer(.2), "timeout")
+    $Door/HingeJoint.queue_free()
+    $Door.mode = RigidBody.MODE_RIGID
+    $Door.gravity_scale = -.5
+    $Door.apply_torque_impulse(Vector3(0, 0, -.2))
